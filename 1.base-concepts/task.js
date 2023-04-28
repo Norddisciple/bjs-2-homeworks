c90e1df8ac40e196e1bd739c6dab38497e3807d7
Cogan <coganizz@gmail.com>
2023-04-28 18:36:34 +0300

    Помощь

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) 
+    let arr = [];
+
+  if (d == 0) {
+    -b / (2 * a)
+  } else if (D > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a),
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+  }
+
   return arr;
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
+
