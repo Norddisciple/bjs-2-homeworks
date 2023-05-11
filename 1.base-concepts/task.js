df96568a9b1a870f828a5323b43701d3953df170
Cogan <coganizz@gmail.com>
2023-05-11 17:50:05 +0300

    Решено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let arr = [];
 
-  let d = Math.pow(b, 2) - 4 * a * c;
+  let D = Math.pow(b, 2) - 4 * a * c;
 
-  if (d < 0) 
-    let arr = [];
-
-  if (d == 0) {
-    -b / (2 * a)
+  if (D < 0) {
+    return arr;
+  } else if (D == 0) {
+    let X = -b / (2 * a);
+    return arr = [X];
   } else if (D > 0) {
-    x1 = (-b + Math.sqrt(d)) / (2 * a),
-    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    return arr = [x1, x2]
   }
-
-  return arr;
 }
 
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-
+  let P = (percent / 100) / 12;
+  let S = amount - contribution;
+  let payMonth = S * (P + (P / ((Math.pow((1 + P), countMonths)) - 1)));
+  let totalPayment = (payMonth * countMonths).toFixed(2);
+  return Number(totalPayment);
 }
 
