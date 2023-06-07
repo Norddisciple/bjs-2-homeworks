349d88c2327a99588e6ce4a00c07af03d3110acb
Cogan <coganizz@gmail.com>
2023-06-07 19:49:37 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount(num) {
+    let parseResult = Number.parseFloat(num);
+    if (Number.isNaN(parseResult)) {
+        throw new Error("Невалидное значение");
+    } return parseResult;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (Error) {
+        return Error;
+    }
+}
+
+
+class Triangle {
+    constructor(side1, side2, side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        if (side1 > side2 + side3 || side2 > side1 + side3 || side3 > side1 + side2) {
+            throw new Error("Треугольник с такими сторонами не существует")
+        }
+    }
+
+    get perimeter() {
+        return this._perimeter = this.side1 + this.side2 + this.side3;
+    }
+
+    get area() {
+        let halfPerimeter = this._perimeter / 2;
+        return this._area = Number(Math.sqrt((halfPerimeter) *
+            (halfPerimeter - this.side1) *
+            (halfPerimeter - this.side2) *
+            (halfPerimeter - this.side3)).toFixed(3));
+    }
+}
+
+
+function getTriangle(side1, side2, side3) {
+        try {
+            return new Triangle(side1, side2, side3);
+        } catch  {
+            return {
+                get area() { 
+                    return "Ошибка! Треугольник не существует" },
+                get perimeter() {
+                     return "Ошибка! Треугольник не существует" }
+            }
+        } 
+    }
+
+
